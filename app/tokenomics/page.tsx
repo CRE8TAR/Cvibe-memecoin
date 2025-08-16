d1e4d945e5e14312b02d094fd6b722cc95f83d95
C8 <cre8tar.tech@gmail.com>
2025-08-16 20:38:41 +0530

    Update page.tsx

--- app/tokenomics/page.tsx
+++ app/tokenomics/page.tsx
@@ -691,7 +691,7 @@ export default function TokenomicsPage() {
             <div className="flex flex-col gap-6 py-10 md:flex-row md:items-center md:justify-between">
               <div className="flex items-center gap-3">
                 <Image
-                  src="/placeholder.svg?height=32&width=32"
+                  src="public/images/cvibecoin.png"
                   width={32}
                   height={32}
                   alt="Token Logo"
